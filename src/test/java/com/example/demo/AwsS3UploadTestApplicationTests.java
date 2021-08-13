85102ef5580bbc03c0b67f31769354d25dbaa9ee
jee00609 <integral76@daum.net>
2021-08-13 18:10:01 +0900

    1st Commit

---
+++ src/test/java/com/example/demo/AwsS3UploadTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3UploadTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
